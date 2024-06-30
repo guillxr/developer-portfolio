1f3bcfe241554070d3757380ce1d635b2dcfd326
guillxr <136463238+guillxr@users.noreply.github.com>
2024-06-30 12:05:46 -0300

    update nav mobile

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -3,3 +3,23 @@ window.addEventListener('scroll', () => {
     let scrollLimit = window.innerWidth > 834 ? 100 : 40;
     nav.classList.toggle('scroll', window.scrollY > scrollLimit);
 });
+
+window.addEventListener('scroll', function() {
+    var currentSection = '';
+    var sections = document.querySelectorAll('section');
+    sections.forEach(function(section) {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (window.scrollY >= sectionTop - sectionHeight / 3) {
+            currentSection = section.getAttribute('id');
+        }
+    });
+
+    var navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(function(link) {
+        link.classList.remove('active');
+        if (link.getAttribute('href').substring(1) === currentSection) {
+            link.classList.add('active');
+        }
+    });
+});
